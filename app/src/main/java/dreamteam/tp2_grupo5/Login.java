9438ce4ba59a8e773dd63f1538e519c4b079b299
Jsesan <jsesan00@gmail.com>
2021-10-19 23:24:11 -0300

    [TP2-Android] Layouts for Login and Register

--- app/src/main/java/dreamteam/tp2_grupo5/Login.java
+++ app/src/main/java/dreamteam/tp2_grupo5/Login.java
@@ -2,7 +2,9 @@ package dreamteam.tp2_grupo5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+
+    public void onRegistration(View view){
+        Intent intent = new Intent(Login.this, Registration.class);
+        startActivity(intent);
     }
 }
