26f9a6d5cac1c8af15ef640433855fc73828d180
Jsesan <jsesan00@gmail.com>
2021-10-19 21:29:53 -0300

    [Android TP2] pattern ready

--- app/src/main/java/dreamteam/tp2_grupo5/MainActivity.java
+++ app/src/main/java/dreamteam/tp2_grupo5/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.andrognito.patternlockview.PatternLockView;
+
+import dreamteam.tp2_grupo5.pattern.PatternListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    String patternKey = "012";
+    PatternLockView patternLockView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        patternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
+        PatternListener patternListener = new PatternListener();
+        patternListener.setPatternLockView(patternLockView);
+        patternListener.setKey(patternKey);
+        patternListener.setContext(MainActivity.this);
+        patternLockView.addPatternLockListener(patternListener);
     }
 }
---
+++ app/src/main/java/dreamteam/tp2_grupo5/pattern/PatternListener.java
@@ -0,0 +1,57 @@
+package dreamteam.tp2_grupo5.pattern;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+
+import java.util.List;
+
+import dreamteam.tp2_grupo5.Login;
+
+public class PatternListener implements PatternLockViewListener {
+    PatternLockView patternLockView;
+    String key;
+    Context context;
+    @Override
+    public void onStarted() {
+
+    }
+
+    @Override
+    public void onProgress(List<PatternLockView.Dot> progressPattern) {
+
+    }
+
+    @Override
+    public void onComplete(List<PatternLockView.Dot> pattern) {
+        String finalPattern = PatternLockUtils.patternToString(patternLockView, pattern);
+        if(finalPattern.equals(key)){
+            Toast.makeText(context, "Correcto :)", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(context, Login.class);
+            context.startActivity(intent);
+        }else{
+            Toast.makeText(context, "Incorrecto :(", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onCleared() {
+
+    }
+
+    public void setPatternLockView(PatternLockView patternLockView) {
+        this.patternLockView = patternLockView;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+}
