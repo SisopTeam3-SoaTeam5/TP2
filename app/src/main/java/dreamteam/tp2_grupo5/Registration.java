044da8b706805f57e4494fcb389c24e5f75a3733
SebasMarmol <sebastian.marmol@virtualdreams.io>
2021-10-23 16:47:38 -0300

    Strings toast

--- app/src/main/java/dreamteam/tp2_grupo5/Registration.java
+++ app/src/main/java/dreamteam/tp2_grupo5/Registration.java
@@ -42,87 +42,88 @@ public class Registration extends AppCompatActivity {
 
     }
 
-    public void registerHandler(View view){
-        boolean ok = true;
+    public void registerHandler(View view) {
 
         String emailText = email.getText().toString();
+        String showError = "";
         ValidationState validateEmail = validateEmail(emailText);
-        if(!validateEmail.getStatus()){
-            ok = false;
-            Toast.makeText(Registration.this, validateEmail.getMessage(), Toast.LENGTH_SHORT).show();
-        }
+        if (!validateEmail.getStatus())
+            showError = addErrorText(showError, validateEmail.getMessage());
+
         String passwordText = password.getText().toString();
         ValidationState validatePassword = validatePassword(passwordText, repeatedPassword);
-
-        if(!validatePassword.getStatus()){
-            ok= false;
-            Toast.makeText(Registration.this, validatePassword.getMessage() , Toast.LENGTH_SHORT).show(); //Chequear como mostrar ambos toast si suceden al mismo tiempo.
-        }
+        if (!validatePassword.getStatus())
+            showError = addErrorText(showError, validatePassword.getMessage());
 
         String commissionText = commission.getText().toString();
+        if (!commissionText.equals("2900") && !commissionText.equals("3900"))
+            showError = addErrorText(showError, "Commission should be 2900 or 3900");
 
-        if(!commissionText.equals("2900") && !commissionText.equals("3900")){
-            ok = false;
-            Toast.makeText(Registration.this, "Commission should be 2900 or 3900", Toast.LENGTH_SHORT).show();
-        }
         String nameText = name.getText().toString();
         String lastnameText = lastname.getText().toString();
         String dniText = dni.getText().toString();
         String groupText = group.getText().toString();
 
 
-        if(nameText.isEmpty() || lastnameText.isEmpty() || dniText.isEmpty() || groupText.isEmpty()){
-            ok = false;
-            Toast.makeText(Registration.this, "Fields should not be empty", Toast.LENGTH_SHORT).show();
-        }
+        if (nameText.isEmpty() || lastnameText.isEmpty() || dniText.isEmpty() || groupText.isEmpty())
+            showError = addErrorText(showError, "Fields should not be empty");
 
 
-        Map<String,String> values = new HashMap<>();
-        values.put("env","TEST");
-        values.put("name",nameText);
-        values.put("lastname",lastnameText);
-        values.put("dni",dniText);
+        if (!showError.equals(""))
+            Toast.makeText(Registration.this, showError, Toast.LENGTH_SHORT).show();
+
+
+        Map<String, String> values = new HashMap<>();
+        values.put("env", "TEST");
+        values.put("name", nameText);
+        values.put("lastname", lastnameText);
+        values.put("dni", dniText);
         values.put("email", emailText);
-        values.put("password", passwordText );
+        values.put("password", passwordText);
         values.put("commission", commissionText);
-        values.put("group",groupText);
+        values.put("group", groupText);
 
-        if(ok) {
-            HttpPost task = new HttpPost(values);
-            task.execute("http://so-unlam.net.ar/api/api/register");
+        HttpPost task = new HttpPost(values);
+        task.execute("http://so-unlam.net.ar/api/api/register");
 //            if(task.getStatusCode() == 200){
 //                Toast.makeText(Registration.this, "Welcome!", Toast.LENGTH_SHORT).show();
 //                Intent intent = new Intent(Registration.this, Homepage.class);
 //                startActivity(intent);
 //            } Mover esto
-        }
     }
 
-    private ValidationState validateEmail(String emailText){
+    private ValidationState validateEmail(String emailText) {
 
-        if(emailText.isEmpty()){
+        if (emailText.isEmpty()) {
             return new ValidationState(false, "Email should not be empty");
         }
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
             return new ValidationState(false, "Should be a valid email");
         }
 
-        return new ValidationState(true,"Success");
+        return new ValidationState(true, "Success");
     }
 
-    private ValidationState validatePassword(String passwordText, EditText repeatedPassword){
+    private ValidationState validatePassword(String passwordText, EditText repeatedPassword) {
         int minimumLength = 8; // <--Change this to a local env
 
-        if(passwordText.isEmpty() || passwordText.length() < minimumLength ){
+        if (passwordText.isEmpty() || passwordText.length() < minimumLength) {
             return new ValidationState(false, "Password should be at least 8 char long");
         }
 
-        if(!passwordText.equals(repeatedPassword.getText().toString())) {
+        if (!passwordText.equals(repeatedPassword.getText().toString())) {
             System.out.println(passwordText + "!=" + repeatedPassword.getText().toString());
-            return new ValidationState(false,"Doesn't match");
+            return new ValidationState(false, "Doesn't match");
         }
 
-        return new ValidationState(true,"Success") ;
+        return new ValidationState(true, "Success");
+    }
+
+    private String addErrorText(String err, String msg) {
+        if (!err.equals(""))
+            err += System.lineSeparator();
+        return err + msg;
+
     }
 }
