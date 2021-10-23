910250ce42b60dcd72ad0e303081b04ea6f1ca77
Jsesan <jsesan00@gmail.com>
2021-10-23 20:07:34 -0300

    [TP2-Android] some registration refactor

---
+++ app/src/main/java/dreamteam/tp2_grupo5/Constants.java
@@ -0,0 +1,21 @@
+package dreamteam.tp2_grupo5;
+
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Constants {
+    public final static String baseUrl = "http://so-unlam.net.ar/api/api/";
+    public final static int passwordMinLength = 8;
+    public final static String commissionValidationError = "Commission should be 2900 or 3900";
+    public final static String fieldsValidationError = "Fields should not be empty";
+    public final static String emailValidationError = "Should be a valid email";
+    public final static String emptyEmail = "Email should not be empty";
+    public final static String passwordLengthMsg = "Password should be at least 8 char long";
+    public final static String passwordMatchError = "Doesn't match";
+    public final static List<String> commissionValues = Arrays.asList("2900","3900");
+    public final static String successMsg = "Success";
+    public final static String testEnv = "TEST";
+    public final static String prodEnv = "PROD";
+}
