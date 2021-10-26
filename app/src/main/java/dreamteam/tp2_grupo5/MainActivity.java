80f4eb803f967bdf7ba49a72f255ec73bdaa8242
SebasMarmol <sebastian.marmol@virtualdreams.io>
2021-10-26 20:58:43 -0300

    BatteryBar red

--- app/src/main/java/dreamteam/tp2_grupo5/MainActivity.java
+++ app/src/main/java/dreamteam/tp2_grupo5/MainActivity.java
@@ -1,9 +1,15 @@
 package dreamteam.tp2_grupo5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.andrognito.patternlockview.PatternLockView;
 
@@ -13,11 +19,20 @@ public class MainActivity extends AppCompatActivity {
 
     String patternKey = "012";
     PatternLockView patternLockView;
+    TextView batteryText;
+    ProgressBar batteryBar;
+    Intent batteryStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        batteryText = findViewById(R.id.batteryText);
+        batteryBar = findViewById(R.id.batteryBar);
+
+
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        batteryStatus = registerReceiver(null, ifilter);
 
         patternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
         PatternListener patternListener = new PatternListener();
@@ -27,4 +42,18 @@ public class MainActivity extends AppCompatActivity {
         patternLockView.addPatternLockListener(patternListener);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level * 100 / (float) scale;
+        String valueText=(int) batteryPct + "%";
+        batteryBar.setProgress((int) batteryPct);
+        batteryText.setText(valueText);
+        Log.i("Debug", batteryPct + "");
+
+    }
 }
